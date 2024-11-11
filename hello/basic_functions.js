caa485784ad37d3a429c70ab17c3c7c7c7ebfea1
Naomi <naomismith97@yahoo.co.uk>
2024-11-11 12:04:58 +0000

    add ;

--- hello/basic_functions.js
+++ hello/basic_functions.js
@@ -1,12 +1,13 @@
 // This function accepts no arguments
 const printMessage1 = () => {
     console.log('Hello World!');
-    }
+    };
 
 // This function accepts one parameter
 const printMessage2 = (message) => {
     console.log(message);
-    }
+    };
+
 // A function must be called before it runs:
 printMessage2('Hello World!');
 
