e095d8ec1ae3ae0d6279687141b5c7014d533158
Naomi <naomismith97@yahoo.co.uk>
2024-11-12 11:10:43 +0000

    fizzBuzz challenge

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,22 @@
+// file: fizzbuzz.js
+
+// Write a fizzBuzz function in JavaScript. As a reminder, this function should take a number as argument, and then either:
+
+// Return "Fizz" if this number is divisible by 3.
+// Return "Buzz" if this number is divisible by 5.
+// Return "FizzBuzz" if this number is divisible by both 5 and 3.
+// Otherwise, Return the number itself.
+
+const fizzBuzz = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (num % 3 === 0) {
+        return 'Fizz';
+    } else if (num % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return num;
+    }
+}
+
+module.exports = fizzBuzz;
